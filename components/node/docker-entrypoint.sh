6246925d50550a593937d3e9d34e6bf44fe24b7e
Tom Havlik <thavlik@protonmail.com>
2021-09-13 21:44:28 -0500

    ...

---
+++ components/node/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+cardano-node run \
+    --config $CARDANO_CONFIG_PATH/testnet-config.json \
+    --database-path $CARDANO_DB_PATH \
+    --socket-path $CARDANO_DB_PATH/node.socket \
+    --host-addr 172.17.0.2 \
+    --port 1337 \
+    --topology $CARDANO_CONFIG_PATH/testnet-topology.json
