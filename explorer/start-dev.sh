b6f8ab030281802e1ff0cf681a035c44a35d4ec3
Tom Havlik <thavlik@protonmail.com>
2021-09-19 11:20:20 -0500

    ...

--- explorer/start-dev.sh
+++ explorer/start-dev.sh
@@ -3,6 +3,13 @@ set -euo pipefail
 cd "$(dirname "$0")"
 IMAGE=node:16.9.1-stretch-slim
 PORT=8080
-docker kill explorer > /dev/null 2>&1 || true
-docker run --rm -d --name explorer -v $(pwd):/mnt -p $PORT:$PORT $IMAGE bash -c cd /mnt && npm run start-dev
+NAME=explorer
+docker kill $NAME > /dev/null 2>&1 || true
+docker run --rm -d \
+    --name $NAME \
+    -v $(pwd):/mnt \
+    -p $PORT:$PORT \
+    $IMAGE \
+    bash -c cd /mnt && npm run start-dev
 echo "Development server is hosted at http://localhost:$PORT"
+docker logs -f $NAME
