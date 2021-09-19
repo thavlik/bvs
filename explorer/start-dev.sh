24b0f8ed5f3fe89eb57efb138323a68e9a4694dc
Tom Havlik <thavlik@protonmail.com>
2021-09-19 10:58:56 -0500

    ...

--- explorer/start-dev.sh
+++ explorer/start-dev.sh
@@ -2,5 +2,8 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 IMAGE=thavlik/bvs-explorer:latest
+PORT=8080
 docker build -t $IMAGE .
-docker run --rm -d --name explorer -v $(pwd):/mnt -p 8080:8080 $IMAGE
+docker kill explorer > /dev/null 2>&1 || true
+docker run --rm -d --name explorer -v $(pwd):/mnt -p $PORT:$PORT $IMAGE
+echo "Development server is hosted at http://localhost:$PORT"
