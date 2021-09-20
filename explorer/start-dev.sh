8ab769c4d8d4fbe404f13fc9f7662bfbcfee5b28
Tom Havlik <thavlik@protonmail.com>
2021-09-20 16:24:01 -0500

    ...

--- explorer/start-dev.sh
+++ explorer/start-dev.sh
@@ -5,6 +5,13 @@ IMAGE=node:16.9.1-stretch-slim
 PORT=8080
 NAME=explorer
 docker kill $NAME >/dev/null 2>&1 || true
-docker run --rm --name $NAME -it -v $(pwd):/mnt -p $PORT:$PORT $IMAGE bash -c cd /mnt && /usr/local/bin/npm i && /usr/local/bin/npm run start-dev
+docker run --rm \
+    --name $NAME \
+    -it \
+    -v $(pwd):/mnt \
+    --entrypoint /bin/sh \
+    -p $PORT:$PORT \
+    $IMAGE \
+    -c "cd /mnt && /usr/local/bin/npm i && /usr/local/bin/npm run start-dev"
 echo "Development server is hosted at http://localhost:$PORT"
 docker logs -f $NAME
